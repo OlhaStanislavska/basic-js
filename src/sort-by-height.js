533c8b8326ad24f826bc65939a9505e67205b525
Olha Stanislavska <olga.stanislavskaya@gmail.com>
2023-04-15 00:09:22 +0300

    feat: add sort-by-height.js, get-email-domain.js, mac-address.js

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  const index = arr.reduce((acc, el, idx) => {
+    if (el === -1) acc.push(idx);
+    return acc;
+  }, []);
+  let res = arr.filter(el => el !== -1).sort((a, b) => a - b);
+  index.forEach(ind => res.splice(ind, 0, -1));
+  return res;
 }
 
 module.exports = {
