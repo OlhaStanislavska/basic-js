6278971278a9f8e41be93c23a05ab39c1749a614
Olha Stanislavska <olga.stanislavskaya@gmail.com>
2023-04-14 23:17:15 +0300

    feat: add transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -11,11 +11,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ * ['--discard-prev', 1, 2, 3],
+*  ['--double-prev', 1, 2, 3],
+*  [1, 2, 3, '--double-next'],
+*  [1, 2, 3, '--discard-next']
+*  input: [1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5], output: [1, 2, 3, 4, 5]
+*  input: [1, 2, 3, '--double-next', 1337, '--double-prev', 4, 5], output: [1, 2, 3, 1337, 1337, 1337, 4, 5]
+*  input: [1, 2, 3, '--discard-next', 1337, '--discard-prev', 4, 5], output: [1, 2, 3, 4, 5]
+*  input: [1, 2, 3, '--double-next', 1337, '--discard-prev', 4, 5], output: [1, 2, 3, 1337, 4, 5]
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!");
+  const res = [];  
+  for (let i = 0; i < arr.length; i++) {
+    if (!isNaN(arr[i])) {
+      if (arr[i + 1] === '--double-prev') {
+        res.push(arr[i]);
+        res.push(arr[i]);
+      } else if (arr[i + 1] === '--discard-prev') {
+        continue;
+      } else {
+        res.push(arr[i]);
+      }
+    } else if (arr[i] === '--double-next' && (i + 1) < arr.length) {
+      res.push(arr[i + 1]);
+    } else if (arr[i] === '--double-next' && i === (arr.length - 1)) {
+      continue;
+    } else if (arr[i] === '--discard-prev') {
+      continue;
+    } else if (arr[i] === '--double-prev') {
+      continue;
+    } else if (arr[i] === '--discard-next') {
+      i++;
+    } else if (isNaN(arr[i])) {
+      res.push(arr[i]);
+    }
+  }
+  return res;
 }
 
 module.exports = {
